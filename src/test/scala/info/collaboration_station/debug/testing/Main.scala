ceee728d239afefdd160134926ce16fbd55bbcbf
JohnReed <johnreed@johnreed-Galago-UltraPro>
2016-03-14 22:02:58 -0400

    moved Main into test folder so Main doesn't end up in the published library.

--- src/test/scala/info/collaboration_station/debug/testing/Main.scala
+++ src/test/scala/info/collaboration_station/debug/testing/Main.scala
@@ -1,17 +1,19 @@
-package info.collaboration_station.test
+package info.collaboration_station.debug.testing
 
+import info.collaboration_station.debug._
 /**
   * Created by johnreed on 3/14/16.
   */
-import info.collaboration_station.debug._
 
 /**
   * Once you type on the Scala console in IntelliJ Community Edition,
   * hit Ctrl+Enter instead of enter to execute.
   */
 object Main {
-  // changed name from "main" to "mainT" for ease of use as library
-  def mainT(args: Array[String]) {
+  /**
+    * run this by doing "sbt test:run"
+    */
+  def main(args: Array[String]) {
     "Hello World 1".trace()
     "Hello World 2".traceStdOut
     Debug.trace("Hello World 3")
